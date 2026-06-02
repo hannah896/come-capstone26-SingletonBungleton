87378aaff12f2f3432a05a0696e2db386aa910fe
hannah896 <20222025@o365.hanbat.ac.kr>
2026-06-03 04:51:27 +0900

    Fix: 채광 모션 오류

--- Assets/02_Scripts/03_Entity/Player/PlayerInventory.cs
+++ Assets/02_Scripts/03_Entity/Player/PlayerInventory.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.Actions.MenuPriority;
 
 /// <summary>
 /// 플레이어가 직접 들고 있는 인벤토리입니다.
@@ -614,7 +615,7 @@ public class PlayerInventory : MonoBehaviour
             return false;
 
         int damage = Mathf.Max(1, Mathf.RoundToInt(handItem.attackDamage));
-        var ctx = new DamageContext(gameObject, hit.point, damage, handItem.itemID);
+        var ctx = new DamageContext(gameObject, hit.point, damage, handItem.itemID, actionType, handItem.harvestableNodeTypes);
         return node.CanDamage(ctx);
     }
 
