6fbb9eca7c7d9563747e206d50ee07b69378fdaf
Gyeongbo Kim <101711272+GyeongboKim@users.noreply.github.com>
2026-09-10 20:49:30 +0900

    WIP: 어드레서블 주소

--- Assets/02_Scripts/@Scripts/Scenes/GameScene.cs
+++ Assets/02_Scripts/@Scripts/Scenes/GameScene.cs
@@ -185,7 +185,7 @@ public class GameScene : SceneBase
             new GameObject(nameof(NetworkPlayerJoinNotice)).AddComponent<NetworkPlayerJoinNotice>();
         }
 
-        await Main.UI.ShowHudOverlay<UI_Hud_WorldState>("UI_WorldState");
+        await Main.UI.ShowHudOverlay<UI_Hud_WorldState>("UI_Hud_WorldState");
 
         // TODO: 소환된 플레이어가 자신의 HUD(UI_Hud_Game)를 띄우는 단계 (다음 작업)
     }
